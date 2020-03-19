eec8b1f239aa46ff51017c8fc134eac9f054245d
StephaneGRETA <62358919+StephaneGRETA@users.noreply.github.com>
2020-03-19 17:05:21 +0100

    Update jeu.js

--- jeu/public/jeu.js
+++ jeu/public/jeu.js
@@ -1,5 +1,8 @@
 /* sélection des éléments de la page pour travailler avec */
 var airDeJeu = document.getElementById("air_de_jeu")
+//inputs contient tous les élements html de class 'aTrouver'
+//inputs est un tableau qui contient plusieurs valeurs
+//ici tous les inputs de class 'aTrouver'
 var inputs = airDeJeu.getElementsByClassName("aTrouver")
 var jeu = document.getElementById("jeu")
 var message = document.getElementById("message")
@@ -7,10 +10,14 @@ var affichage = document.getElementById("affichage")
 var fin = document.getElementById("fin")
 
 /* déclaration des variables */
+// message à afficher dans la <p id="message">
 var solution = "Demandez un nouveau jeu"
+//compter le nombre d'erreur. Pour dire perdu au bout de 7 erreurs
 var etape = 0
+//pour compter le nombre de bonne lettre et dire gagner au bon moment
 var bravo = 0
 //var mots = "garage"
+//liste des mots à trouver, un est tiré au hasard
 var mots=["svelte", "hacker", "bifide", "bonsai", "cheval"]
 
 /* déclaration des fonctions */
@@ -23,19 +30,35 @@ function getNiveau(){
 }
 
 //fonction pour activer ou desactiver les champs de saisie
+//ok=false
 function desactiver(nodes, ok){
+    //couleur de dégrisage
     var couleur = "#ffffff"
 
+    //ok est vrai si il faut griser
+    // si ok=false pas de if
     if(ok)
     {
+        //couleur de grisage
         couleur = "#302535"
     }
 
+    //si pas de if, alors couleur="#ffffff"
+
+    //modifie le background_color des input text
     var style = "background-color: " + couleur
 
+    //boucle pour modifier tous les inputs d'un coup
+    //il y en 7 en tout
+    //c'est quoi nodes ? tableau qui contient tous les inputs text
+    //le for va de 0 à la taille maxi de nodes (nodes.length)
     for (var i = 0; i < nodes.length; i++) {
+        //efface toutes les lettres dans les inputs text
         nodes[i].value = ""
+        //applique le bon background color
         nodes[i].setAttribute("style", style)
+        //bloque la saisie
+        //disabled = false
         nodes[i].disabled = ok
     }
 }
@@ -63,6 +86,7 @@ var niveau = getNiveau()
 document.getElementById("lettre6").hidden = true
 
 //Liste de mots pour le niveau difficile
+// ici on tire un mot de 7 pour le niveau difficile
 if (niveau == 7)
 {
     mots = ["absolue","butanol","cabotin","gabarie","piccolo"]
@@ -83,7 +107,12 @@ jeu.addEventListener("click", function(){
     var indice = Math.floor(Math.random() * max)
     solution = mots[indice]
 
+    //appel à la fonction desactiver. On lui passe en paramètres inputs et false
+    //false valeur bool
+    //du coup ok = 'false'
+    //nodes = inputs
     desactiver(inputs, false)
+    //
     affichage.src = "./public/jeu00.png"
     message.innerHTML = ""
     message.setAttribute("style", "color: #302535")
@@ -102,20 +131,24 @@ for (var i = 0; i < inputs.length; i++) {
         var position = this.getAttribute("id").charAt(6)
         var saisie = this.value
         var lettre = solution.charAt(position)
-
         
         if(saisie.toLowerCase() == lettre.toLowerCase())
         {
             bravo++
 
+            //c'est ici qu'on gère le niveau 7
             if (bravo == niveau)
             {
                 gagner(inputs, solution)
             }
 
         }else{
+            //etape = etape+1
             etape++
+            //modifie le src de img
+            //car la var affichage contient la balise img
             affichage.src = './public/jeu0'+etape+'.png'
+            //à la première erreur, src = './public/jeu01.png'
 
             if(etape > 6){
                 perdre(inputs, solution)
@@ -123,8 +156,3 @@ for (var i = 0; i < inputs.length; i++) {
         }
     })
 }
-
-
-
-
-
